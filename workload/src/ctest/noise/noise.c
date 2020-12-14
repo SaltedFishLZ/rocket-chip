fc89b79e7421c03b5db94d689bccef8a1a094724
SaltedFishLZ <zhliang@berkeley.edu>
2020-12-14 02:51:53 +0000

    other workloads using magic stamps updated

--- workload/src/ctest/noise/noise.c
+++ workload/src/ctest/noise/noise.c
@@ -8,18 +8,16 @@
 volatile float * float_in = (volatile float *)0x81000000; 
 volatile float * float_out = (volatile float *)0x82000000; 
 
-volatile uint32_t * magic = (volatile uint32_t *)0x8FFF0000; 
-
 
 int main()
 {
     // setStats(1);
     for (int i = 0; i < ITERS; i++) {
-        *magic = 0x149a;
+magic_start_stamp(0)
         // begin
         *float_out = (*float_in) + (rand()/(float)RAND_MAX) * 0.2;
         // end
-        *magic = 0x249a;
+magic_end_stamp(0)
     }
     // setStats(0);
     return 0;
